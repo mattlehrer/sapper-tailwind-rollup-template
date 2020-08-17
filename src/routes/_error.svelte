7b4960de6c814180c2c9f797cfdb627998791526
Matt Lehrer <lehrerm@gmail.com>
2020-08-16 17:18:16 -0700

    update styles

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -5,36 +5,14 @@
 	const dev = process.env.NODE_ENV === 'development';
 </script>
 
-<style>
-	h1, p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
 	<title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<h1 class="mx-auto my-2 text-5xl font-bold sm:text-6xl">{status}</h1>
 
-<p>{error.message}</p>
+<p class="mx-auto my-4">{error.message}</p>
 
 {#if dev && error.stack}
-	<pre>{error.stack}</pre>
+<pre>{error.stack}</pre>
 {/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,13 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1 class="text-center text-3xl uppercase font-bold mb-2 mx-auto">
+<h1 class="mx-auto mb-2 text-5xl font-bold text-center uppercase sm:text-6xl">
 	Great success!
 </h1>
 
-<figure class="mb-4 text-center mx-auto">
+<figure class="mx-auto mb-4 text-center">
 	<img
-		class="mb-4 max-w-sm w-100 mx-auto"
+		class="max-w-sm mx-auto mb-4 w-100"
 		alt="Success Kid"
 		src="successkid.jpg"
 	/>
